8353f71f112767021ac00b8e339b9d92fcd21290
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-05-11 21:35:35 +0300

    add mobile-menu.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-import './js/modal-order';
+import './js/modal-order.js';
 import './js/modal-thank.js';
+import './js/mobile-menu.js';
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
