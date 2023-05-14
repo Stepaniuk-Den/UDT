d5b5a4ab3352eb32bc11bca1912e2de48fdb4b51
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-05-14 12:38:18 +0300

    add js to mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,26 @@
-
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileLinks = document.querySelectorAll('.mobile-menu-link');
+  const lockBody = document.body;
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    lockBody.classList.toggle('lock-body');
+    mobileMenuRef.classList.toggle('is-open');
+  });
+
+  for (let i = 0; i < mobileLinks.length; i += 1) {
+    mobileLinks[i].addEventListener('click', () => {
+      mobileMenuRef.classList.remove('is-open');
+      menuBtnRef.classList.remove('is-open');
+      lockBody.classList.toggle('lock-body');
+      const expand =
+        menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+      menuBtnRef.setAttribute('aria-expanded', !expand);
     });
-  })();
+  }
+})();
