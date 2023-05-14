1a0818726eae1cfd084c0143c673cb89ea1060e7
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-05-14 21:02:51 +0300

    add js to thanks

--- src/js/modal-thank.js
+++ src/js/modal-thank.js
@@ -1,14 +1,18 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[thanks-data-modal-open]"),
-    closeModalBtn: document.querySelector("[thanks-data-modal-close]"),
-    modal: document.querySelector("[thanks-data-modal]"),
+    openModalBtn: document.querySelector('[thanks-data-modal-open]'),
+    closeModalBtn: document.querySelector('[thanks-data-modal-close]'),
+    modal: document.querySelector('[thanks-data-modal]'),
+    form: document.querySelector('.contacts-form'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("thanks-is-hidden");
+    refs.modal.classList.toggle('thanks-is-hidden');
   }
+  refs.form.addEventListener('submit', event => {
+    event.preventDefault();
+    refs.modal.classList.toggle('thanks-is-hidden');
+  });
 })();
